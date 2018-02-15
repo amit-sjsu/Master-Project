5c0ed12f400ea1492e27e1f614e1fc1d8b59a759
Anshul Agrawal <anshul.dec7@gmail.com>
2018-02-15 00:56:29 -0800

    Added the Chart with updated UI

--- code/app.py
+++ code/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, send_file, render_template
+import xlrd
+import csv
+import json
 
 
 app = Flask(__name__)
@@ -6,6 +9,72 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return send_file("templates/index.html")
+
+@app.route('/dd')
+def userAgeData():
+    workbook = xlrd.open_workbook('/Users/anshul/Documents/LinkedIn Profile/AGE/AGE02.xls')
+    worksheet = workbook.sheet_by_name('Sheet5')
+    worksheet1 = workbook.sheet_by_name('Sheet7')
+    census_albama_25_29 = worksheet.cell(2, 15).value
+    census_california_25_29 = worksheet.cell(192, 15).value
+    census_florida_25_29 = worksheet.cell(331, 15).value
+    census_new_york_25_29 = worksheet.cell(1863, 15).value
+    census_albama_30_34 = worksheet1.cell(2, 11).value
+    census_california_30_34 = worksheet1.cell(192, 11).value
+    census_florida_30_34 = worksheet1.cell(331, 11).value
+    census_new_york_30_34 = worksheet1.cell(1863, 11).value
+    cal_count_25_29 = 0
+    albama_count_25_29 = 0
+    ny_count_25_29 = 0
+    fl_count_25_29 = 0
+    cal_count_30_34 = 0
+    albama_count_30_34 = 0
+    ny_count_30_34 = 0
+    fl_count_30_34 = 0
+    with open('/Users/anshul/Documents/CMPE295A/ICPSR_35074/DS0001/35074-0001-Data.tsv', 'r') as tsvin:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+        for row in tsvin:
+            if row[15] == '6' and row[2] == '6':
+                cal_count_25_29 = cal_count_25_29+1
+            if row[15] == '1' and row[2] == '6':
+                albama_count_25_29 = albama_count_25_29+1
+            if row[15] == '12' and row[2] == '6':
+                fl_count_25_29 = fl_count_25_29+1
+            if row[15] == '36' and row[2] == '6':
+                ny_count_25_29 = ny_count_25_29+1
+            if row[15] == '6' and row[2] == '7':
+                cal_count_30_34 = cal_count_30_34+1
+            if row[15] == '1' and row[2] == '7':
+                albama_count_30_34 = albama_count_30_34+1
+            if row[15] == '12' and row[2] == '7':
+                ny_count_30_34 = ny_count_30_34+1
+            if row[15] == '36' and row[2] == '7':
+                fl_count_30_34 = fl_count_30_34+1
+
+    print(census_albama_25_29)
+    print(census_california_25_29)
+    print(census_florida_25_29)
+    print(census_new_york_25_29)
+    print(cal_count_25_29)
+    print(albama_count_25_29)
+    print(fl_count_25_29)
+    print(ny_count_25_29)
+    data={}
+    data["CA"]=[{"age_25_29":{"census_data":census_california_25_29, "drug_data":cal_count_25_29}},
+                {"age_30_34":{"census_data":census_california_30_34, "drug_data":cal_count_30_34}}]
+
+    data["AL"] = [{"age_25_29": {"census_data": census_albama_25_29, "drug_data": albama_count_25_29}},
+                  {"age_30_34": {"census_data": census_albama_30_34, "drug_data": albama_count_30_34}}]
+
+    data["FL"] = [{"age_25_29": {"census_data": census_florida_25_29, "drug_data": fl_count_25_29}},
+                  {"age_30_34": {"census_data": census_florida_30_34, "drug_data": fl_count_30_34}}]
+
+    data["NY"] = [{"age_25_29": {"census_data": census_new_york_25_29, "drug_data": ny_count_25_29}},
+                  {"age_30_34": {"census_data": census_new_york_30_34, "drug_data": ny_count_30_34}}]
+
+    return json.dumps(data);
+
+
 
 
 @app.route('/index')
@@ -17,5 +86,9 @@ def charts(chartID = 'chart_ID', chart_type = 'bar', chart_height = 550):
 	yAxis = {"title": {"text": 'Probability'}}
 	return render_template('charts.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
+
+
+
 if __name__ == '__main__':
+    userAgeData()
     app.run(port=5003);

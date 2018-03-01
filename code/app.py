65658c78538e34839a640e80465012e8b87eec48
Anshul Agrawal <anshul.dec7@gmail.com>
2018-03-01 00:19:10 -0800

    Added Features

--- code/app.py
+++ code/app.py
@@ -12,9 +12,7 @@ def index():
 
 @app.route('/dd')
 def userAgeData():
-
-    workbook = xlrd.open_workbook('/Users/Harshit/LECTURES/295B/Code/Master-Project/code/AGE02.xls')
-
+    workbook = xlrd.open_workbook('/Users/anshul/Documents/LinkedIn Profile/AGE/AGE02.xls')
     worksheet = workbook.sheet_by_name('Sheet5')
     worksheet1 = workbook.sheet_by_name('Sheet7')
     census_albama_25_29 = worksheet.cell(2, 15).value
@@ -39,8 +37,7 @@ def userAgeData():
     ny_count = 0
     fl_count = 0
 
-    with open('/Users/Harshit/LECTURES/295B/Code/Master-Project/code/35074-0001-Data.tsv', 'r') as tsvin:
-
+    with open('/Users/anshul/Documents/CMPE295A/ICPSR_35074/DS0001/35074-0001-Data.tsv', 'r') as tsvin:
         tsvin = csv.reader(tsvin, delimiter='\t')
         for row in tsvin:
             if row[15] == '6' and row[2] == '6':
@@ -70,7 +67,7 @@ def userAgeData():
                 ny_count = ny_count+1
 
 
-    workbook1 = xlrd.open_workbook('/Users/Harshit/LECTURES/295B/Code/Master-Project/code/AGE01.xls')
+    workbook1 = xlrd.open_workbook('/Users/anshul/Documents/LinkedIn Profile/AGE/AGE01.xls')
     worksheet2 = workbook1.sheet_by_name('Sheet1')
     census_albama = worksheet2.cell(2, 15).value
     census_california = worksheet2.cell(192, 15).value
@@ -90,32 +87,76 @@ def userAgeData():
     print(fl_count_25_29)
     print(ny_count_25_29)
 
+    print(cal_count)
+    print(albama_count)
+    print(fl_count)
+    print(ny_count)
 
-    data = [
-         {'CA': [{'P_DrugAddict': 123},
-                {'P_DA_Age_25_29': 123,
-                'P_DA_Age_30_34': 123}
-           ]
-          },
-         {'AL': [{'P_DrugAddict': 223},
-                {'P_DA_Age_25_29': 223,
-                'P_DA_Age_30_34': 223}
-           ]
-         },
-        {'NY': [{'P_DrugAddict': 323},
-                {'P_DA_Age_25_29': 323,
-                'P_DA_Age_30_34': 323}
-           ]
-         },
-        {'FL': [{'P_DrugAddict': 423},
-                {'P_DA_Age_25_29': 423,
-                'P_DA_Age_30_34': 423}
-           ]
-         }
-    ];
+    data={}
+    data['CA']=[{'age_25_29':{'census_data':census_california_25_29, 'drug_data':cal_count_25_29}},
+                {'age_30_34':{'census_data':census_california_30_34, 'drug_data':cal_count_30_34}}]
 
-    return data;
+    data['AL'] = [{'age_25_29': {'census_data': census_albama_25_29, 'drug_data': albama_count_25_29}},
+                  {'age_30_34': {'census_data': census_albama_30_34, 'drug_data': albama_count_30_34}}]
 
+    data['FL'] = [{'age_25_29': {'census_data': census_florida_25_29, 'drug_data': fl_count_25_29}},
+                  {'age_30_34': {'census_data': census_florida_30_34, 'drug_data': fl_count_30_34}}]
+
+    data['NY'] = [{'age_25_29': {'census_data': census_new_york_25_29, 'drug_data': ny_count_25_29}},
+                  {'age_30_34': {'census_data': census_new_york_30_34, 'drug_data': ny_count_30_34}}]
+
+    p_ca_drug_addict = cal_count/census_california
+    p_ca_25_29_da = cal_count_25_29/cal_count
+    p_ca_25_29 = census_california_25_29/census_california
+
+    p_ca_30_34_da = cal_count_30_34/cal_count
+    p_ca_30_34 = census_california_30_34/census_california
+
+    p_da_ca_25_29 = p_ca_drug_addict * p_ca_25_29_da / p_ca_25_29
+    p_da_ca_30_34 = p_ca_drug_addict * p_ca_30_34_da / p_ca_30_34
+
+    p_al_drug_addict = albama_count/census_albama
+    p_al_25_29_da = albama_count_25_29/albama_count
+    p_al_25_29 = census_albama_25_29/census_albama
+    p_al_30_34_da = albama_count_30_34/albama_count
+    p_al_30_34 = census_albama_30_34/census_albama
+
+    p_da_al_25_29 = p_al_drug_addict * p_al_25_29_da / p_al_25_29
+    p_da_al_30_34 = p_al_drug_addict * p_al_30_34_da / p_al_30_34
+
+
+    p_ny_drug_addict = ny_count/census_new_york
+    p_ny_25_29_da = ny_count_25_29/ny_count
+    p_ny_25_29 = census_new_york_25_29/census_new_york
+    p_ny_30_34_da = ny_count_30_34/ny_count
+    p_ny_30_34 = census_new_york_30_34/census_new_york
+
+    p_da_ny_25_29 = p_ny_drug_addict * p_ny_25_29_da / p_ny_25_29
+    p_da_ny_30_34 = p_ny_drug_addict * p_ny_30_34_da / p_ny_30_34
+
+    p_fl_drug_addict = fl_count/census_florida
+    p_fl_25_29_da = fl_count_25_29/fl_count
+    p_fl_25_29 = census_florida_25_29/census_florida
+    p_fl_30_34_da = fl_count_30_34/fl_count
+    p_fl_30_34 = census_florida_30_34/census_florida
+
+    p_da_fl_25_29 = p_fl_drug_addict * p_fl_25_29_da / p_fl_25_29
+    p_da_fl_30_34 = p_fl_drug_addict * p_fl_30_34_da / p_fl_30_34
+
+
+
+
+
+    print(p_da_ca_25_29)
+    print(p_da_ca_30_34)
+    print(p_da_al_25_29)
+    print(p_da_al_30_34)
+    print(p_da_ny_25_29)
+    print(p_da_ny_30_34)
+    print(p_da_fl_25_29)
+    print(p_da_fl_30_34)
+
+    return json.dumps(data);
 
 
 
@@ -129,13 +170,6 @@ def charts(chartID = 'chart_ID', chart_type = 'bar', chart_height = 550):
 	yAxis = {"title": {"text": 'Probability'}}
 	return render_template('charts.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
-
-
-@app.route('/addictionProbability',methods=['GET'])
-def getAddictionProbability():
-    data = userAgeData();
-    print (data);
-    return render_template('analysis.html', result=data);
 
 
 
